f7fe1ac6a35e148ff9316844740ae2d72f30a033
Mizuki Nakano <mizukin1187@gmail.com>
2024-09-11 05:28:43 +0900

    Done testing

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("FERNET_KEY") or "hard to guess string"
     CELERY = {
-        "broker_url": os.environ.get("REDIS_URL"),
-        "result_backend": os.environ.get("REDIS_URL")
+        "broker_url": os.environ.get("REDIS_URL") or "redis://localhost:6379/0",
+        "result_backend": os.environ.get("REDIS_URL") or "redis://localhost:6379/0"
     }
 
 
--- ink-scheduler.py
+++ ink-scheduler.py
@@ -1,12 +1,10 @@
 from app import create_flask
+from app.tasks import get_response
 # from app.utils.util import calendar_start_sync
 # calendar_start_sync() # I dont think I should start the sync here. I gotta start it rather when a user sign up
 
 app = create_flask()
 celery = app.extensions["celery"]
-app.app_context().push()    #Not sure I need this but Youtuber says I do
-
-
 
 @app.shell_context_processor
 def make_shell_context():
